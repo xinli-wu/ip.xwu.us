516ca47d4546d42437bd529daf526e47fb8a0ee2
Xinli WU <ryan7gm@gmail.com>
2022-04-03 22:34:57 +1000

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import { Map } from './pages/Map';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
+}
+
---
+++ src/pages/Map.tsx
@@ -0,0 +1,28 @@
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import './Map.css';
+
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+export const Map = () => {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
