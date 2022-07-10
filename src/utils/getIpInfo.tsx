e9d5f365696868f5dbbadde267d09451a562c966
Xinli WU <ryan7gm@gmail.com>
2022-07-11 01:44:09 +1000

    added .env REACT_APP_API_ADDR

--- src/utils/getIpInfo.tsx
+++ src/utils/getIpInfo.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const getIpInfo = async (ipDomain: string) => {
-  const res = await axios.get(`https://api.ip.xwu.us/${ipDomain || ''}`).catch(err => console.error(err));
+  const res = await axios.get(`${process.env.REACT_APP_API_ADDR}/${ipDomain || ''}`).catch(err => console.error(err));
   return res?.data;
 };
